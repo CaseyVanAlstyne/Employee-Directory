9529a2fdb373294a2c89e3d52e8e70380cf6d704
Casey VanAlstyne <caseywvanalstyne@gmail.com>
2020-08-04 18:59:08 -0400

    added imports to employeeDir

---
+++ employee-directory/src/pages/employeeDir.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+import EmployeeList from "../components/employeelist";
+import Footer from "../components/footer";
+import Header from "../components/header";
+import Search from "../components/search";
+import $ from "jquery";
