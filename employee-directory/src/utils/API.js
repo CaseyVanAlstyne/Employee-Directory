ea1536b28392256ed13e20b5ee66dbdfc24b026e
Casey VanAlstyne <caseywvanalstyne@gmail.com>
2021-01-10 17:00:44 -0500

    restructuring project, started with the api call

--- employee-directory/src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default {
-  getAllEmployees: function (query) {
-    return axios.get("https://randomuser.me/api/?results=10");
-  },
-};
-
-export default API;
